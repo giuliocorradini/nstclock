3df86b6ffdf39671f0c6bea2ff6a44ca0f20f34b
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-04-19 14:24:31 +0200

    Moved BMP functions

--- lib/i2c/i2c.c
+++ lib/i2c/i2c.c
@@ -14,3 +14,79 @@ void i2c_configure() {
     ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_1, &config));
     i2c_driver_install(I2C_NUM_1, I2C_MODE_MASTER, 128, 128, 0);
 }
+
+/*!
+ *  @brief Function that creates a mandatory delay required in some of the APIs such as "bmg250_soft_reset",
+ *      "bmg250_set_foc", "bmg250_perform_self_test"  and so on.
+ *
+ *  @param[in] period_ms  : the required wait time in milliseconds.
+ *  @return void.
+ *
+ */
+void delay_ms(uint32_t period_ms)
+{
+    vTaskDelay(1000 / portTICK_RATE_MS);
+}
+
+/*!
+ *  @brief Function for writing the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[in] reg_data : Pointer to the data buffer whose value is to be written.
+ *  @param[in] length   : No of bytes to write.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+    esp_err_t err;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    
+    //  Write pointer to register (it'll be read in the next transaction)
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (i2c_addr << 1 | I2C_MASTER_WRITE), true);
+    i2c_master_write_byte(cmd, reg_addr, true);
+    i2c_master_write(cmd, reg_data, length, true);
+    i2c_master_stop(cmd);
+
+    err = i2c_master_cmd_begin(I2C_NUM_1, cmd, 200 / portTICK_RATE_MS);
+
+    return err;
+}
+
+/*!
+ *  @brief Function for reading the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : Sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[out] reg_data    : Pointer to the data buffer to store the read data.
+ *  @param[in] length   : No of bytes to read.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+    esp_err_t err;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    
+    //  Write pointer to register (it'll be read in the next transaction)
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (i2c_addr << 1 | I2C_MASTER_WRITE), true);
+    i2c_master_write_byte(cmd, reg_addr, true);
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (i2c_addr << 1 | I2C_MASTER_READ), true);
+    i2c_master_read(cmd, reg_data, length, I2C_MASTER_LAST_NACK);
+    i2c_master_stop(cmd);
+
+    err = i2c_master_cmd_begin(I2C_NUM_1, cmd, 200 / portTICK_RATE_MS);
+
+    return err;
+}
--- lib/i2c/i2c.h
+++ lib/i2c/i2c.h
@@ -4,5 +4,7 @@
 #include <stdint.h>
 
 void i2c_configure();
+int8_t i2c_bmp_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+int8_t i2c_bmp_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
 
 #endif
