3f90250166fdfdbc50070f76d14993a488bbbc30
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-04-26 15:13:13 +0200

    Generate big fonts

--- fonts/convert.sh
+++ fonts/convert.sh
@@ -6,6 +6,26 @@ function activate_venv {
     fi
 }
 
+function generate_big {
+    cd fonts
+    for font in $(ls *.ttf); do
+        if [[ "$font" =~ ^big_* ]]; then
+            echo $font
+        else
+            cp "$font" "big_$(basename $font .ttf).ttf"
+        fi
+    done
+    cd ..
+}
+
+function clean_big {
+    cd fonts
+    for font in $(ls big_*); do
+        rm $font
+    done
+    cd ..
+}
+
 function clean_dashes {
     cd fonts
     for font in $(ls *.ttf); do
@@ -18,10 +38,26 @@ function convert_ttf {
     for font in $(ls fonts/*.ttf); do
         echo "Converting $font"
         header="$(basename $font .ttf).h"
-        python3 ./fonts/lcdgfx/tools/fontgenerator.py --ttf "$font" -f new -s 12 > "$header"
+        if [[ "$font" =~ ^fonts/big_* ]]; then
+            python3 ./fonts/lcdgfx/tools/fontgenerator.py --ttf "$font" -f new -s 20 > "$header"
+        else
+            python3 ./fonts/lcdgfx/tools/fontgenerator.py --ttf "$font" -f new -s 12 > "$header"
+        fi
         mv "$header" "./lib/fonts/"
     done
 }
 
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+    activate_venv
+fi
+
+if [[ "$1" == "--bigfonts" ]]; then
+    generate_big
+fi
+
 clean_dashes
 convert_ttf
+
+if [[ "$1" == "--bigfonts" ]]; then
+    clean_big
+fi
